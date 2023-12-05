1e665fb1b9916bcfbb7737e022bb1a1ee5934df3
Starz0r <starz0r@starz0r.com>
2023-12-05 02:48:19 -0600

    main: streams can be removed

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import asyncio
 import os
 import sys
-from typing import Dict, Final
+from typing import Dict, Final, Optional, Tuple
 
 import disnake
 import structlog
@@ -11,6 +11,14 @@ from twitchAPI.eventsub.webhook import EventSubWebhook
 from twitchAPI.helper import first
 from twitchAPI.object.eventsub import StreamOfflineEvent, StreamOnlineEvent
 from twitchAPI.twitch import Twitch
+
+RETCODE: int = 0
+EVLOOP: Final[asyncio.AbstractEventLoop] = asyncio.new_event_loop()
+LOGGER: Final[structlog.stdlib.BoundLogger] = structlog.getLogger()
+# [twitch name, msg]
+ACTIVE_NOTIFICATIONS: Dict[str, disnake.message.Message] = {}
+# [discord user id, [twitch name, topic id, topic id]]
+REGISTERED_STREAMS: Dict[str, Tuple[str, str, str]] = {}
 
 TWITCH_APP_ID: Final[str] = os.environ.get("TWITCH_APP_ID")
 TWITCH_APP_SECRET: Final[str] = os.environ.get("TWITCH_APP_SECRET")
@@ -22,20 +30,16 @@ DISCORD_CMD_SYNC_FLAGS: Final[
     commands.CommandSyncFlags] = commands.CommandSyncFlags.default()
 DISCORD_CMD_SYNC_FLAGS.sync_commands_debug = True
 DISCORD_CMD_SYNC_FLAGS.sync_commands = True
-DISCORD_CMD_SYNC_FLAGS.allow_command_deletion = True
+DISCORD_CMD_SYNC_FLAGS.allow_command_deletion = False
 DISCORD_BOT: commands.InteractionBot = commands.InteractionBot(
-    command_sync_flags=DISCORD_CMD_SYNC_FLAGS)
+    command_sync_flags=DISCORD_CMD_SYNC_FLAGS, loop=EVLOOP)
 DISCORD_TOKEN: Final[str] = os.environ.get("DISCORD_TOKEN")
 DISCORD_CHANNEL: Final[str] = os.environ.get("DISCORD_CHANNEL")
-
-RETCODE: int = 0
-EVLOOP: Final[asyncio.AbstractEventLoop] = asyncio.new_event_loop()
-LOGGER: Final[structlog.stdlib.BoundLogger] = structlog.getLogger()
-MSGS: Dict[str, disnake.message.Message] = {}
+DISCORD_MOD_ROLE: Final[str] = os.environ.get("DISCORD_MOD_ROLE")
 
 
 async def on_stream_offline(payload: StreamOfflineEvent):
-    msg = MSGS.pop(payload.event.broadcaster_user_name)
+    msg = ACTIVE_NOTIFICATIONS.pop(payload.event.broadcaster_user_name)
     await msg.delete()
 
 
@@ -50,27 +54,87 @@ async def on_stream_online(payload: StreamOnlineEvent):
         )
     msg = await ch.send(
         f"https://twitch.tv/{payload.event.broadcaster_user_name}")
-    MSGS.update({f"{payload.event.broadcaster_user_name}": msg})
+    ACTIVE_NOTIFICATIONS.update(
+        {f"{payload.event.broadcaster_user_name}": msg})
 
 
-@DISCORD_BOT.slash_command(dm_permission=False,
-                           name="add_stream",
-                           auto_sync=True)
-async def add_stream(ctx: disnake.ApplicationCommandInteraction, stream: str):
+@DISCORD_BOT.slash_command(description="Remove stream from the watchlist.")
+async def remove_stream(ctx: disnake.ApplicationCommandInteraction,
+                        user: Optional[disnake.User]) -> None:
+    if user is not None:
+        if ctx.author.get_role(int(DISCORD_MOD_ROLE)) is not None:
+            if str(user.id) in REGISTERED_STREAMS:
+                name, topic_a, topic_b = REGISTERED_STREAMS.pop(str(user.id))
+                # TODO: also remove live notifications if there are any
+                await asyncio.gather(
+                    TWITCH_EVENTSUB.unsubscribe_topic(topic_a),
+                    TWITCH_EVENTSUB.unsubscribe_topic(topic_b),
+                )
+                LOGGER.info("unregistering stream",
+                            stream=name,
+                            topics=(topic_a, topic_b))
+                return await ctx.send(
+                    "♻ Stream was removed from the watchlist.")
+    if str(ctx.author.id) in REGISTERED_STREAMS:
+        name, topic_a, topic_b = REGISTERED_STREAMS.pop(str(ctx.author.id))
+        # TODO: also remove live notifications if there are any
+        await asyncio.gather(
+            TWITCH_EVENTSUB.unsubscribe_topic(topic_a),
+            TWITCH_EVENTSUB.unsubscribe_topic(topic_b),
+        )
+        LOGGER.info("unregistering stream",
+                    stream=name,
+                    topics=(topic_a, topic_b))
+        return await ctx.send("♻ Stream was removed from the watchlist.")
+    return await ctx.send("Stream was not on the watchlist, nothing was done.")
+
+
+@DISCORD_BOT.slash_command(description="Add stream to the watchlist.")
+async def add_stream(ctx: disnake.ApplicationCommandInteraction,
+                     stream: str) -> None:
     twitch_user = await first(TWITCH.get_users(logins=[stream]))
     if twitch_user is None:
-        return await ctx.response.send_message(
-            f"could not find Twitch User with matching name of: {stream}.")
-    await asyncio.gather(
+        return await ctx.send(
+            f"Could not find Twitch User with matching name of: {stream}.")
+
+    # check if discord user already registered a stream
+    # TODO: also remove live notifications if there are any
+    # QUEST: should mods also be bound to the same limitations?
+    if str(ctx.author.id) in REGISTERED_STREAMS:
+        name, topic_a, topic_b = REGISTERED_STREAMS.pop(str(ctx.author.id))
+        LOGGER.info(
+            f"user {name} already registered a stream, in-placing...",
+            topics=(topic_a, topic_b),
+        )
+        await asyncio.gather(
+            TWITCH_EVENTSUB.unsubscribe_topic(topic_a),
+            TWITCH_EVENTSUB.unsubscribe_topic(topic_b),
+        )
+
+    topic_a, topic_b = await asyncio.gather(
         TWITCH_EVENTSUB.listen_stream_online(twitch_user.id, on_stream_online),
         TWITCH_EVENTSUB.listen_stream_offline(twitch_user.id,
                                               on_stream_offline),
     )
+    LOGGER.info("registering stream",
+                stream=twitch_user.id,
+                topics=(topic_a, topic_b))
+    REGISTERED_STREAMS.update(
+        {f"{ctx.author.id}": (twitch_user.display_name, topic_a, topic_b)})
+
+    return await ctx.send("🎥SStream added to the watchlist!")
+
+
+@DISCORD_BOT.event
+async def on_ready():
+    LOGGER.info("bot is ready")
 
 
 async def main() -> int:
     global TWITCH
     global TWITCH_EVENTSUB
+
+    # logging.basicConfig(level=logging.DEBUG)
 
     LOGGER.info("connecting to the Twitch API")
     TWITCH = await Twitch(TWITCH_APP_ID, TWITCH_APP_SECRET)
@@ -80,11 +144,9 @@ async def main() -> int:
     TWITCH_EVENTSUB.start()
 
     # run discord bot until interrupted
-    LOGGER.info("all ready, running Discord bot until interrupted")
+    LOGGER.info("all ready, running Discord bot until interrupted", )
     try:
-        t = EVLOOP.create_task(DISCORD_BOT.start(DISCORD_TOKEN,
-                                                 reconnect=True))
-        await t
+        await DISCORD_BOT.start(DISCORD_TOKEN, reconnect=True)
     except KeyboardInterrupt:
         LOGGER.info("stop called! shutting down...")
     finally:
@@ -95,4 +157,5 @@ async def main() -> int:
 
 
 if __name__ == "__main__":
+    # DISCORD_BOT.run(DISCORD_TOKEN)
     sys.exit(EVLOOP.run_until_complete(main()))
